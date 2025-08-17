0e4739f0807188e9d02e422e7461af1e7456cac6
Ashutoshbirje <ashutoshbirje4@gmail.com>
2025-08-18 01:59:51 +0530

    ok

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import Component from './components/Component';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <Component/> */}
+       <h1>Welcome to CampusHire</h1>
+    </div>
+  );
+}
+
+export default App;
